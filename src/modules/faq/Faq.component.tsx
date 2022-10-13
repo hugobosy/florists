3b634e07bc25fe3fa5903be9644fe39d69f93353
HugoBosy <hkliszcz@gmail.com>
2022-10-13 10:59:07 +0200

    add title section and wrapper with styles

--- src/modules/faq/Faq.component.tsx
+++ src/modules/faq/Faq.component.tsx
@@ -1,8 +1,21 @@
 import React from "react";
+import { HeaderSection } from "../../components/header-section/HeaderSection.component";
+import { Container } from "@mui/material";
+import S from './Faq.styles';
 
 export const Faq = () => {
 
   return (
-    <h1>FAQ</h1>
-  )
-}
+    <S.Wrapper>
+      <Container>
+        <HeaderSection
+          captionTitle="FAQ"
+          captionHeader="Najczęściej zadawane pytania"
+          size="small"
+          align="center"
+          isLine
+        />
+      </Container>
+    </S.Wrapper>
+  );
+};
